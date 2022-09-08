681fb52cd54051f8d405ca49f2fe200c339c2f41
davbalabekyan <balabekyandavo@gmai.com>
2022-09-08 13:50:05 +0400

    Delete comment

--- src/test/java/core/JsonServer.java
+++ src/test/java/core/JsonServer.java
@@ -6,6 +6,6 @@ public class JsonServer {
 
     @SneakyThrows
     public static void startJsonServer() {
-//        Runtime.getRuntime().exec("json-server --watch db.json");
+        Runtime.getRuntime().exec("json-server --watch db.json");
     }
 }
